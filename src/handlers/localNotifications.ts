3b865cda03f639754b53b9ad433d83464de501cd
Attah David <jocheattahdavid@gmail.com>
2024-10-05 21:24:07 +0100

    Update localNotifications.ts

--- src/handlers/localNotifications.ts
+++ src/handlers/localNotifications.ts
@@ -1,60 +1,73 @@
-// import notifee from '@notifee/react-native';
+import notifee from '@notifee/react-native';
 
-// interface channelData {
-//   id: string;
-//   name: string;
-// }
+interface channelData {
+  id: string;
+  name: string;
+}
 
-// export const createChannel = async (
-//   id: string = 'default',
-//   name: string = 'default channel',
-// ) => {
-//   // Create a channel
-//   const channelId = await notifee.createChannel({
-//     id: id,
-//     name: name,
-//   });
-//   return channelId;
-// };
+export const createChannel = async (
+  id: string = 'default',
+  name: string = 'default channel',
+) => {
+  // Create a channel
+  const channelId = await notifee.createChannel({
+    id: id,
+    name: name,
+  });
+  return channelId;
+};
 
-// export const onDisplayNotification = async (
-//   title: string,
-//   body: string,
-//   channelData: channelData,
-// ) => {
-//   const channelId = await createChannel(channelData.id, channelData.name);
-//   // Display a notification
-//   await notifee.displayNotification({
-//     title: title,
-//     body: body,
-//     android: {
-//       channelId,
-//     },
-//   });
-// };
+export const onDisplayNotification = async (
+  title: string,
+  body: string,
+  channelData: channelData,
+) => {
+  const channelId = await createChannel(channelData.id, channelData.name);
+  // Display a notification
+  await notifee.displayNotification({
+    title: title,
+    body: body,
+    android: {
+      channelId,
+    },
+  });
+};
 
-// export const billsNotification = async (message: string) => {
-//   try {
-//     const noti = await onDisplayNotification('Bill Payment', message, {
-//       id: 'Bills',
-//       name: 'Bills Channel',
-//     });
-//     return noti;
-//   } catch (e) {
-//     console.log(e);
-//     throw e;
-//   }
-// };
+export const billsNotification = async (message: string) => {
+  try {
+    const noti = await onDisplayNotification('Bill Payment', message, {
+      id: 'Bills',
+      name: 'Bills Channel',
+    });
+    return noti;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
 
-// export const transfersNotification = async (message: string) => {
-//   try {
-//     const noti = await onDisplayNotification('💰💰💰💰💰💰', message, {
-//       id: 'Transfers',
-//       name: 'Transfers Channel',
-//     });
-//     return noti;
-//   } catch (e) {
-//     console.log(e);
-//     throw e;
-//   }
-// };
+export const transfersNotification = async (message: string) => {
+  try {
+    const noti = await onDisplayNotification('💰💰💰💰💰💰', message, {
+      id: 'Transfers',
+      name: 'Transfers Channel',
+    });
+    return noti;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
+
+export const myLocationNotification = async (message: string) => {
+  try {
+    const noti = await onDisplayNotification('My Location', message, {
+      id: 'Location',
+      name: 'Location Channel',
+    });
+    return noti;
+  } catch (e) {
+    console.log(e);
+    throw e;
+  }
+};
