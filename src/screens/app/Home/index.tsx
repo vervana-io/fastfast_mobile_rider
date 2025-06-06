47f79e69aac634737b0da4c4f303c536f95e1d5f
Zucci-Daniel <zuccizucci555@gmail.com>
2025-06-06 21:15:59 +0100

    commented out a temp line

--- src/screens/app/Home/index.tsx
+++ src/screens/app/Home/index.tsx
@@ -64,7 +64,7 @@ export const Home = observer((props: HomeProps) => {
   const [appState, setAppState] = useState<AppStateStatus>(
     AppState.currentState,
   );
-  const [shouldCheckPerms, setShouldCheckPerms] = useState(false);
+  // const [shouldCheckPerms, setShouldCheckPerms] = useState(false);
 
   const isFocussed = useIsFocused();
 
